3871ee6ae5ae6eca564d1c5ec9da43731f1437ef
Dan Collins <dcollinsn@gmail.com>
2023-05-04 18:05:08 -0400

    Add update directives to autorefresh.js

--- autorefresh.js
+++ autorefresh.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Melee Autorefresh
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Refresh the list of tournaments, players, or matches every 30 seconds on mtgmelee.
 // @author       Dan Collins <dcollins@batwing.tech>
+// @updateURL    https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/autorefresh.js
+// @downloadURL  https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/autorefresh.js
 // @website      https://github.com/dcollinsn/melee-tampermonkey
 // @match        https://mtgmelee.com/Hub/View/*
 // @match        https://mtgmelee.com/Tournament/Control/*
