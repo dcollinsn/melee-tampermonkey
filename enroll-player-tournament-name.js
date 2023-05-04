84c452929f01ce1573914c8ad6a20a21c2c06bd1
Dan Collins <dcollinsn@gmail.com>
2023-05-04 18:05:40 -0400

    Add update directives to melee enroll player event name script

--- enroll-player-tournament-name.js
+++ enroll-player-tournament-name.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Melee Event Name in Enroll Player Modal
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Show the current tournament name in the header of the "enroll player" modal on mtgmelee.
 // @author       Dan Collins <dcollins@batwing.tech>
+// @updateURL    https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/enroll-player-tournament-name.js
+// @downloadURL  https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/enroll-player-tournament-name.js
 // @website      https://github.com/dcollinsn/melee-tampermonkey
 // @match        https://mtgmelee.com/Tournament/Control/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mtgmelee.com
