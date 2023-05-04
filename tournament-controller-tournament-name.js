a5ccb97be462323b7bed6df323c9f2508cd97ae9
Dan Collins <dcollinsn@gmail.com>
2023-05-04 18:06:12 -0400

    Add update directives to tournament controller event name script

--- tournament-controller-tournament-name.js
+++ tournament-controller-tournament-name.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Melee Event Name in Tournament Controller
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Add tournament name to the page title on the mtgmelee Tournament Controller.
 // @author       Dan Collins <dcollins@batwing.tech>
+// @updateURL    https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/tournament-controller-tournament-name.js
+// @downloadURL  https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/tournament-controller-tournament-name.js
 // @website      https://github.com/dcollinsn/melee-tampermonkey
 // @match        https://mtgmelee.com/Tournament/Control/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mtgmelee.com
