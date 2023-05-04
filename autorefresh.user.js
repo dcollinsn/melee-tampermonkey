109efb5e8f49807545889d3546a723c6945c51cd
Dan Collins <dcollinsn@gmail.com>
2023-05-04 18:15:37 -0400

    Update and rename autorefresh.js to autorefresh.user.js

--- autorefresh.user.js
+++ autorefresh.user.js
@@ -4,8 +4,8 @@
 // @version      1.0.1
 // @description  Refresh the list of tournaments, players, or matches every 30 seconds on mtgmelee.
 // @author       Dan Collins <dcollins@batwing.tech>
-// @updateURL    https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/autorefresh.js
-// @downloadURL  https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/autorefresh.js
+// @updateURL    https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/autorefresh.user.js
+// @downloadURL  https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/autorefresh.user.js
 // @website      https://github.com/dcollinsn/melee-tampermonkey
 // @match        https://mtgmelee.com/Hub/View/*
 // @match        https://mtgmelee.com/Tournament/Control/*
