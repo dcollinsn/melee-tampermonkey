ea549738f2ad1c272125aea60fb8588f18bcc6ed
Dan Collins <dcollinsn@gmail.com>
2023-06-08 21:48:56 -0400

    Update autorefresh.user.js for melee.gg domain name

--- autorefresh.user.js
+++ autorefresh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Melee Autorefresh
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Refresh the list of tournaments, players, or matches every 30 seconds on mtgmelee.
 // @author       Dan Collins <dcollins@batwing.tech>
 // @updateURL    https://raw.githubusercontent.com/dcollinsn/melee-tampermonkey/main/autorefresh.user.js
@@ -9,6 +9,8 @@
 // @website      https://github.com/dcollinsn/melee-tampermonkey
 // @match        https://mtgmelee.com/Hub/View/*
 // @match        https://mtgmelee.com/Tournament/Control/*
+// @match        https://melee.gg/Hub/View/*
+// @match        https://melee.gg/Tournament/Control/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mtgmelee.com
 // @grant        none
 // ==/UserScript==
